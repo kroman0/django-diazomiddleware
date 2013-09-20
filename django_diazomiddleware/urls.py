99fedd7a99f59878772c0c1578da2169ba877e81
Roman Kozlovskyi <krzroman@gmail.com>
2013-09-20 23:41:35 +0300

    pep8

--- django_diazomiddleware/urls.py
+++ django_diazomiddleware/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns
 from os.path import join, dirname
 
 urlpatterns = patterns('',
-    (r'^diazo-debug/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': join(dirname(diazo.__file__), 'debug_resources')}),
-)
+                      (r'^diazo-debug/(?P<path>.*)$', 'django.views.static.serve', {
+                       'document_root': join(dirname(diazo.__file__), 'debug_resources')}),
+                       )
